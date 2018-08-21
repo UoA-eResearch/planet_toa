f5a1036cbd6713dc2c8694e5d46a3cc86dcb2ac0
Nick Young <nick.young@auckland.ac.nz>
2018-08-22 10:40:32 +1200

    add support for optional clobber argument. add support for RapidEye imagery.

--- convert_toa.py
+++ convert_toa.py
@@ -5,26 +5,41 @@ from xml.dom import minidom
 import glob
 import os
 import argparse
+import math
+import datetime
 
 parser = argparse.ArgumentParser(description='Convert Planet TIFs to TOA')
 parser.add_argument('folders', type=str, nargs='+', help='the folders containing TIFs')
+parser.add_argument('-c', '--clobber', action="store_true", help='Whether to overwrite existing toa tiffs')
 args = parser.parse_args()
 
-for folder in args.folders:
-  print("processing " + folder)
+EAI = [1997.8, 1863.5, 1560.4, 1395.0, 1124.4] # Exo-Atmospheric Irradiance as per https://www.planet.com/products/satellite-imagery/files/160625-RapidEye%20Image-Product-Specifications.pdf
+
+for i, folder in enumerate(args.folders):
+  print("processing {}/{} - {}".format(i, len(args.folders), folder))
+  toa_file = glob.glob(os.path.join(folder, "*Analytic_toa.tif"))
+  if toa_file and not args.clobber:
+    print(folder + " already processed")
+    continue
+  tif_file = None
+  mode = None
   try:
     tif_file = glob.glob(os.path.join(folder, "*BGRN_Analytic.tif"))[0]
-    toa_file = glob.glob(os.path.join(folder, "*BGRN_Analytic_toa.tif"))
-    if toa_file:
-      print(folder + " already processed")
-      continue
+    mode = "BGRN"
   except IndexError:
-    print("Couldn't find a BGRN_Analytic tif in {}".format(folder))
+    pass
+  try:
+    tif_file = glob.glob(os.path.join(folder, "*3A_Analytic.tif"))[0]
+    mode = "3A"
+  except IndexError:
+    pass
+  if not tif_file:
+    print("Couldn't find an _Analytic tif in {}".format(folder))
     continue
   try:
-    xml_file = glob.glob(os.path.join(folder, "*BGRN_Analytic_metadata.xml"))[0]
+    xml_file = glob.glob(os.path.join(folder, "*_Analytic_metadata.xml"))[0]
   except IndexError:
-    print("Couldn't find a BGRN_Analytic_metadata xml in {}".format(folder))
+    print("Couldn't find an _Analytic_metadata xml in {}".format(folder))
     continue
 
   try:
@@ -32,15 +47,28 @@ for folder in args.folders:
       bands = src.read().astype(float)
 
     xmldoc = minidom.parse(xml_file)
-    nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
 
+    if mode == "3A":
+      solar_elevation_angle_deg = float(xmldoc.getElementsByTagName("opt:illuminationElevationAngle")[0].firstChild.data)
+      acquisitionDate = xmldoc.getElementsByTagName("re:acquisitionDateTime")[0].firstChild.data
+      dt = datetime.datetime.strptime(acquisitionDate, "%Y-%m-%dT%H:%M:%S.%fZ")
+      day = dt.timetuple().tm_yday
+      sun_distance = 1 - 0.01672 * math.cos(math.radians(0.9856 * (day - 4)))
+      solar_zenith_rad = math.radians(90 - solar_elevation_angle_deg)
+
+    nodes = xmldoc.getElementsByTagNameNS("*", "bandSpecificMetadata")
     # XML parser refers to bands by numbers 1-4
     for node in nodes:
-      bn = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
+      bn = node.getElementsByTagNameNS("*", "bandNumber")[0].firstChild.data
       try:
         i = int(bn) - 1
-        value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
-        bands[i] *= float(value) * 10000
+        if mode == "BGRN":
+          value = node.getElementsByTagNameNS("*", "reflectanceCoefficient")[0].firstChild.data
+          bands[i] *= float(value) * 10000
+        elif mode == "3A":
+          radiometricScaleFactor = float(node.getElementsByTagNameNS("*", "radiometricScaleFactor")[0].firstChild.data)
+          coeff = radiometricScaleFactor * ((math.pi * math.pow(sun_distance, 2)) / (EAI[i] * math.cos(solar_zenith_rad))) * 10000
+          bands[i] *= coeff
       except ValueError:
         print("{} doesn't look like a channel".format(bn))
 
@@ -52,6 +80,5 @@ for folder in args.folders:
 
     with rasterio.open(processed_filename, 'w', **kwargs) as dst:
       dst.write(bands.astype(rasterio.uint16))
-    print("{} done".format(folder))
   except Exception as e:
     print("ERROR! : " + e)
